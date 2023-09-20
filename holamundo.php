8d01b99a4e9648b41b542afdc330f259403ad685
AlxMedit <94053604+AlxMedit@users.noreply.github.com>
2023-09-20 10:07:21 +0200

    Actualización código

--- holamundo.php
+++ holamundo.php
@@ -1,4 +1,4 @@
 <?php
     echo "hola mundo <br>"; # Con esto decimos hola mundo y añadimos salto de línea
 
-    print "<a href='https://github.com/AlxMedit/DWES_CODES'>Pincha para acceder al repositorio</a>"; # Ponemos el hipervínculo a mi repo
+    echo "<a href='https://github.com/AlxMedit/DWES_CODES'>Pincha para acceder al repositorio</a>"; # Ponemos el hipervínculo a mi repo
